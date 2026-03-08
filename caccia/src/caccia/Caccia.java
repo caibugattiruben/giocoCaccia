3f7954c20d28fcf52a074074a8aff30a41c13c28
caibugattiruben <caibugatti.ruben@franchettisalviani.net>
2026-03-08 21:14:30 +0100

    Interfaccia scelta personaggio
    Ho quasi completato l'interfaccia per scegliere il personaggio.

---
+++ caccia/src/caccia/Caccia.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package caccia;
+
+/**
+ *
+ * @author ruben
+ */
+public class Caccia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SceltaPersonaggio p1=new SceltaPersonaggio();
+        p1.setVisible(true);
+    }
+    
+}
